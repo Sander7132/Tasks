680911617b23103f12c9669bca3ee52d0de63fd4
Sander Vinter <sander.vinter@gmail.com>
2023-06-07 12:23:47 +0300

    As a user, I want to be able to edit my tasks [#185253459]

--- functions.js
+++ functions.js
@@ -1,23 +1,23 @@
-const verifier = require('@gradeup/email-verify');
+const verifier = require("@gradeup/email-verify");
 
-
-const verifyEmail = email => new Promise((resolve, reject) => {
+const verifyEmail = (email) =>
+  new Promise((resolve, reject) => {
     verifier.verify(email, (err, info) => {
-        console.log(err, info);
-        if (err) {
-            reject(JSON.stringify(info));
-        } else {
-            resolve(info);
-        }
+      console.log(err, info);
+      if (err) {
+        reject(JSON.stringify(info));
+      } else {
+        resolve(info);
+      }
     });
-});
+  });
 
-const tryToParseJson = jsonString => {
-    try {
-        return JSON.parse(jsonString);
-    } catch {
-        return false;
-    }
+const tryToParseJson = (jsonString) => {
+  try {
+    return JSON.parse(jsonString);
+  } catch {
+    return false;
+  }
 };
 
-module.exports = {tryToParseJson, verifyEmail};
+module.exports = { tryToParseJson, verifyEmail };
